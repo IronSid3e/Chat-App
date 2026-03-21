cc282876d19f2e5cc4ed2c59b06ad2297d86e696
IronSid3e <omertahirustun05@hotmail.com>
2026-03-22 02:33:38 +0300

    set Native bottom tabs and drawer

--- src/app/(drawer)/(home)/(tabs)/_layout.tsx
+++ src/app/(drawer)/(home)/(tabs)/_layout.tsx
@@ -1,5 +1,33 @@
-import { Tabs } from "expo-router"
+import { Tabs } from "expo-router";
+import Ionicons from '@expo/vector-icons/Ionicons';
 
-export default function TabsLayout () {
-    return <Tabs/>
+export default function TabsLayout() {
+  return (
+    <Tabs screenOptions={{
+        tabBarActiveTintColor: '#EA7B7B',
+        headerShown: false
+    }}>
+      <Tabs.Screen
+        name="chats"
+        options={{
+          title: 'Chats',
+          tabBarIcon: ({ color }) => <Ionicons name="chatbox" size={24} color={ color } />
+        }}
+      />
+      <Tabs.Screen
+        name="settings"
+        options={{
+          title: 'Settings',
+          tabBarIcon: ({ color }) => <Ionicons name="settings" size={24} color={ color } />
+        }}
+      />
+      <Tabs.Screen
+        name="search"
+        options={{
+          title: 'Search',
+          tabBarIcon: ({ color }) => <Ionicons name="search" size={24} color={ color } />
+        }}
+      />
+    </Tabs>
+  );
 }
--- src/app/(drawer)/(home)/(tabs)/chats/index.tsx
+++ src/app/(drawer)/(home)/(tabs)/chats/index.tsx
@@ -1,9 +1,10 @@
-import { View, Text } from "react-native";
+import { View, Text, StatusBar } from "react-native";
 
 export default function HomeScreen() {
     return (
         <View className='flex-1 items-center justify-center'>
             <Text className="text-3xl">Channel List</Text>
+            <StatusBar/>
         </View>
     )
 }
